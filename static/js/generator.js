3c166cf1c4456158247c426c2b39ab29b68b8b79
OverLeo007 <sokolov_lev2003@mail.ru>
2023-02-18 23:06:17 +0700

    Upd quotes printing

--- static/js/generator.js
+++ static/js/generator.js
@@ -8,6 +8,65 @@ url = "static/jsons/diogen_quotes.json"
 
 randint = (min, max) => (Math.round(Math.random() * (max - min)) + min)
 
+
+let printQueue = []; // очередь задач для печати
+let isPrinting = false; // флаг, указывающий, идет ли в данный момент печать
+
+function printText(text, elementId) {
+    const task = { text, elementId };
+    printQueue.push(task);
+    if (!isPrinting) {
+        isPrinting = true;
+        printNext();
+    }
+}
+
+function printNext() {
+    if (printQueue.length === 0) {
+        isPrinting = false;
+        return;
+    }
+    const task = printQueue.shift();
+    const { text, elementId } = task;
+    const element = document.getElementById(elementId);
+    let index = 0;
+    const speed = 50; // скорость печати (миллисекунды)
+
+    function type() {
+        element.innerHTML = text.slice(0, index++);
+        if (index > text.length) {
+            setTimeout(printNext, 1000); // задержка перед следующей задачей
+            return;
+        }
+        setTimeout(type, speed);
+    }
+
+    type();
+}
+
+
+
+function machineTyping(text, elementId) {
+    let index = 0;
+
+    function type() {
+        
+        const typedText = document.getElementById(elementId);
+        const currentText = text.slice(0, index);
+        typedText.textContent = currentText;
+        if (currentText[currentText.length - 1] === '\n') {
+            typedText.innerHTML += '<br>';
+        }
+        if (index < text.length) {
+            index++;
+            setTimeout(type, 50);
+        }
+    }
+
+    type();
+}
+
+
 let resp = fetch(url).then(function (response) {
     return response.json()
 })
@@ -16,9 +75,10 @@ print(resp.then(data => {
     let quoteJson = data[qNum]
     let quote = quoteJson["quote"]
     let explanation = quoteJson["explanation"]
-    quoteTag.innerText = quote
-    expTag.innerText = explanation
-    
-    
+    printText(quote, "quote-text")
+    // quoteTag.innerText = quote
+    printText(explanation, "quote-exp")
+
+
 }))
 
